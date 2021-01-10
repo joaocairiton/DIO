358096378754eddb80ef28673c016114ed5068fe
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-10 20:05:18 -0300

    projetoTipoMvcC#

---
+++ cursoMVC/CursoTest/CategoriasControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+
+namespace CursoTest
+{
+   public class CategoriasControllerTest
+    {
+
+       
+
+        public CategoriasControllerTest()
+        {
+
+            
+        }
+    }
+}
